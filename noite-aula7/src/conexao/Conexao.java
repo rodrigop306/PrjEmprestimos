ee1a25210df6def8c46d81da44c8bc10fab7af2b
c_aue <c_aue@10.68.76.106>
2018-11-05 20:18:02 -0200

    Conexão com banco estabelicida.

--- noite-aula7/src/conexao/Conexao.java
+++ noite-aula7/src/conexao/Conexao.java
@@ -5,6 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 //
 public class Conexao {
+	
 	private static Connection conn;
     private static final String USUARIO = "SYSTEM";
     private static final String SENHA = "110964";
--- noite-aula7/src/controller/LoginController.java
+++ noite-aula7/src/controller/LoginController.java
@@ -12,25 +12,32 @@ import model.UsuarioDAOImpl;
 
 @WebServlet("/LoginController")
 public class LoginController extends HttpServlet {
-	
+
 	private static final long serialVersionUID = 1L;
 	private UsuarioDAOImpl emprestimosDAO;
-	
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		doPost(request, response);
 	}
-	
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		emprestimosDAO = new UsuarioDAOImpl();
 		String user = request.getParameter("txtLogin");
 		String senha = request.getParameter("txtPassword");
 		String mensagem = "";
+		System.out.println(user + ", " + senha);
 		HttpSession session = request.getSession();
-		if(emprestimosDAO.pesquisaUsuario(user, senha)){
-			mensagem = "Usuário logado!";
-		} else {
-			mensagem = "Usuário ou senha incorreto";
-		}
+//		try {
+			if (emprestimosDAO.pesquisaUsuario(user, senha)) {
+				mensagem = "Usuário logado!";
+			} else {
+				mensagem = "Usuário ou senha incorreto";
+			}
+		/**} catch (Throwable e) {
+			mensagem = "Opa, deu erro!";
+		}*/
 		session.setAttribute("MENSAGEM", mensagem);
 		response.sendRedirect("./login.jsp");
 	}
